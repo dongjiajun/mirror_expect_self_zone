cf659aa0d3ca49b54848da8021c9f454fda1b6eb
djj <838739252@qq.com>
2022-01-09 18:17:31 +0800

    origin version,first commit.

---
+++ src/com/test/Hello.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @author 董佳俊
+ * @version 1.0
+ * @date 2022/1/9 18:15
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("test transfer");
+    }
+}
